1fcd4220d5c725a1f3a4c6329967ecb433778954
georgesohran <soxrgeorge@gmail.com>
2023-08-31 18:14:42 +0300

    more physics

--- player/Player1.gd
+++ player/Player1.gd
@@ -6,7 +6,6 @@ var acel = 2
 
 var gravity = Physics.Force.new(Vector2.DOWN,100)
 var run = Physics.Force.new(Vector2.RIGHT,speed)
-var enertia = Physics.Force.new(Vector2.RIGHT,1000)
 
 var active_forces = [gravity,run]
 
@@ -15,14 +14,10 @@ func _physics_process(delta):
 	
 	if $Left.is_colliding() or $Right.is_colliding():
 		run.add_aceleration()
-		if not active_forces.has(run):
-			active_forces.append(run)
-			
+
 	else:
 		run.add_friction()
-		gravity.add_aceleration()
-		if not active_forces.has(gravity):
-			active_forces.append(gravity)
+		gravity.add_aceleration(1.07)
 	if Input.is_action_just_pressed("shoot"):
 		active_forces.append(Physics.Force.new(-to_local($Gun.shoot_direction),10))
 		
@@ -30,8 +25,10 @@ func _physics_process(delta):
 		move_vector += force.get_force()
 		if force != run and gravity:
 			force.add_friction()
-		if force.force < 0.1:
-			active_forces.erase(force)
+			if force.force < 0.1:
+				active_forces.erase(force)
+	
+			
 		
 			
 	move_vector.x = clamp(move_vector.x,-MAX_SPEED,MAX_SPEED)
